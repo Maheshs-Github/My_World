c1d447787b6b649e3bbbb7fc19b5080a00ee84f4
Mahesh Mane <maheshmane9075@gmail.com>
2025-11-12 15:23:28 +0530

    Adding the Router Navigation fun

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 const app = express();
 import { configDotenv } from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import Connection from "./Config/DBConn.js";
 import Router from "./Routes/PortFolioRoutes.js";
 import cors from "cors";
@@ -13,5 +16,17 @@ app.use(cors());
 Connection();
 
 app.use("/api/MyWorld/", Router);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// ✅ Serve React build (adjust folder name if needed)
+app.use(express.static(path.join(__dirname, "./FrontEnd/dist")));
+
+// ✅ Catch-all for React Router
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./FrontEnd/dist/index.html"));
+});
+
 
 app.listen(Port, () => console.log(`Listening from the Port ${Port}`));
