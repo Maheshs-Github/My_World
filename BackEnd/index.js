3839024fd098d6695ae02be3b8db82f1a693c410
Mahesh Mane <maheshmane9075@gmail.com>
2025-11-12 17:01:25 +0530

    CoRs Sol

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -16,12 +16,18 @@ app.use(express.json());
 
 const allowedOrigins = [
   "https://my-world-x66a.onrender.com", // your frontend on Render
-  "http://localhost:5173" // for local testing
+  "http://localhost:5174" // for local testing
 ];
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
